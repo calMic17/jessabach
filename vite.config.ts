d2fbaa7cd90474f8418d79c75cff4e3d265a9a4a
Vasanth kumar AM <vasanthkumar17112002@gmail.com>
2026-04-24 08:43:57 +0530

    Confix changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  //need to add this on live site to make sure the assets are loaded correctly
+  // base: '/',
+
   base: process.env.NODE_ENV === 'production' ? '/jessabach/' : '/',
   server: {
     middlewareMode: false,
